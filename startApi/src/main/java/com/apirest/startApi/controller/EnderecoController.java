abcd9f60cdd42a6a781d72ceef5bd1c4bd896ec0
Filipe Leandro <lealves.work@gmail.com>
2023-03-17 20:16:53 -0300

    Endereco: DadosCadastro e Cadastro

--- startApi/src/main/java/com/apirest/startApi/controller/EnderecoController.java
+++ startApi/src/main/java/com/apirest/startApi/controller/EnderecoController.java
@@ -1,15 +1,27 @@
 package com.apirest.startApi.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.apirest.startApi.model.endereco.DadosCadastroEndereco;
+import com.apirest.startApi.model.endereco.Endereco;
+import com.apirest.startApi.repository.EnderecoRepository;
+
+import jakarta.transaction.Transactional;
 
 @RestController
 @RequestMapping("/enderecos")
 public class EnderecoController {
+
+    @Autowired
+    private EnderecoRepository repository;
     
     @PostMapping
-    public void cadastro(){
-
+    @Transactional
+    public void cadastro(@RequestBody DadosCadastroEndereco dados){
+        repository.save(new Endereco(dados));
     }
 }

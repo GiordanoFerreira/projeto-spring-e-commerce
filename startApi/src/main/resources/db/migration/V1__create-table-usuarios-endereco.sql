8f5cd7626e261af3ebb033179ae7441041ed37b6
Filipe Leandro <lealves.work@gmail.com>
2023-03-17 19:17:28 -0300

    add migrations: usuarios, enderecos

---
+++ startApi/src/main/resources/db/migration/V1__create-table-usuarios-endereco.sql
@@ -0,0 +1,27 @@
+create table usuarios(
+
+    id bigint not null auto_increment,
+    nome varchar(50) not null,
+    email varchar(100) not null unique,
+    senha varchar(50) not null,
+    cpf varchar(11) not null unique,
+    ativo tinyint not null,
+
+    primary key(id)
+
+);
+
+create table enderecos(
+
+    id bigint not null auto_increment,
+    logradouro varchar(100) not null,
+    bairro varchar(100) not null,
+    cep varchar(9) not null,
+    complemento varchar(100),
+    numero varchar(20) not null,
+    uf char(2) not null,
+    cidade varchar(100) not null,
+
+    usuario_id bigint,
+    primary key(id)
+);

9f0de5fe133d17b155349b9d0c5f4fc049986a5b
Giordano <giordanopalmezano@gmail.com>
2023-03-19 00:43:23 -0300

    Add listagem de usuarios

--- startApi/src/main/java/com/apirest/startApi/controller/UsuarioController.java
+++ startApi/src/main/java/com/apirest/startApi/controller/UsuarioController.java
@@ -1,12 +1,16 @@
 package com.apirest.startApi.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.apirest.startApi.model.usuario.DadosCadastroUsuario;
+import com.apirest.startApi.model.usuario.DadosListagemUsuario;
 import com.apirest.startApi.model.usuario.Usuario;
 import com.apirest.startApi.repository.UsuarioRepository;
 
@@ -23,5 +27,10 @@ public class UsuarioController {
     @Transactional
     public void cadastro(@RequestBody DadosCadastroUsuario dados) {
         repository.save(new Usuario(dados));
+    }
+
+    @GetMapping
+    public Page<DadosListagemUsuario> listar(Pageable paginacao){
+        return repository.findAll(paginacao).map(DadosListagemUsuario :: new);
     }
 }
---
+++ startApi/src/main/java/com/apirest/startApi/model/usuario/DadosListagemUsuario.java
@@ -0,0 +1,8 @@
+package com.apirest.startApi.model.usuario;
+
+public record DadosListagemUsuario(Long id, String nome, String email, String senha, String cpf, boolean ativo) {
+    
+    public DadosListagemUsuario(Usuario usuario){
+        this(usuario.getId(), usuario.getNome(), usuario.getEmail(), usuario.getSenha(), usuario.getCpf(), usuario.isAtivo());
+    }
+}

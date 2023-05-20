b397d8eaf6e4962e7e7467aac29a69d57739009f
Giordano <giordanopalmezano@gmail.com>
2023-05-19 22:39:16 -0300

    Refatorando Service Usuario

--- startApi/src/main/java/com/apirest/startApi/controllers/UsuarioController.java
+++ startApi/src/main/java/com/apirest/startApi/controllers/UsuarioController.java
@@ -15,8 +15,6 @@ import org.springframework.web.bind.annotation.RestController;
 import com.apirest.startApi.dto.usuario.DadosAtualizacaoUsuarioDto;
 import com.apirest.startApi.dto.usuario.DadosCadastroUsuarioDto;
 import com.apirest.startApi.dto.usuario.DadosListagemUsuarioDto;
-import com.apirest.startApi.models.Usuario;
-import com.apirest.startApi.repository.UsuarioRepository;
 import com.apirest.startApi.services.UsuarioService;
 
 import jakarta.transaction.Transactional;
@@ -27,15 +25,12 @@ import jakarta.validation.Valid;
 public class UsuarioController {
 
     @Autowired
-    private UsuarioRepository repository;
-
-    @Autowired
     private UsuarioService service;
 
     @PostMapping
     @Transactional
     public void cadastro(@RequestBody @Valid DadosCadastroUsuarioDto dados) {
-        repository.save(new Usuario(dados));
+        service.cadastro(dados);
     }
 
     @GetMapping
@@ -47,14 +42,12 @@ public class UsuarioController {
     @PutMapping
     @Transactional
     public void atualizar(@RequestBody @Valid DadosAtualizacaoUsuarioDto dados) {
-        var usuario = repository.getReferenceById(dados.id());
-        usuario.atualizarInformacoes(dados);
+        service.atualizar(dados);
     }
 
     @DeleteMapping("/{id}")
     @Transactional
     public void excluir(@PathVariable Long id){
-        var usuario = repository.getReferenceById(id);
-        usuario.excluir();
+        service.excluir(id);
     }
 }
--- startApi/src/main/java/com/apirest/startApi/services/UsuarioService.java
+++ startApi/src/main/java/com/apirest/startApi/services/UsuarioService.java
@@ -4,9 +4,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 
+import com.apirest.startApi.dto.usuario.DadosAtualizacaoUsuarioDto;
+import com.apirest.startApi.dto.usuario.DadosCadastroUsuarioDto;
 import com.apirest.startApi.dto.usuario.DadosListagemUsuarioDto;
+import com.apirest.startApi.models.Usuario;
 import com.apirest.startApi.repository.UsuarioRepository;
+
+import jakarta.validation.Valid;
 
 @Service
 public class UsuarioService {
@@ -16,5 +23,19 @@ public class UsuarioService {
 
     public Page<DadosListagemUsuarioDto> listar(Pageable paginacao) {
         return repository.findAllByAtivoTrue(paginacao).map(DadosListagemUsuarioDto :: new);
+    }
+
+    public void cadastro(@RequestBody @Valid DadosCadastroUsuarioDto dados){
+        repository.save(new Usuario(dados));
+    }
+
+    public void atualizar(@RequestBody @Valid DadosAtualizacaoUsuarioDto dados){
+        var usuario = repository.getReferenceById(dados.id());
+        usuario.atualizarInformacoes(dados);
+    }
+
+    public void excluir(@PathVariable Long id){
+        var usuario = repository.getReferenceById(id);
+        usuario.excluir();
     }
 }
